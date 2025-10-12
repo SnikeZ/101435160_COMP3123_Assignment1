9278b084c90eb0858f36c42d4d686e38f8a89b12
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-10-12 18:31:19 -0400

    user SignUp implemented

---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const userSchema = require('../models/user');
+const expressValidator = require('express-validator')
+
+exports.signup = async (req, res) => {
+  try {
+
+    const { username, email, password } = req.body;
+
+    console.log(`Username ${username}`)
+    console.log(`Email ${email}`)
+    console.log(`Password ${password}`)
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log(`HashedPassword ${hashedPassword}`)
+
+
+    const newUser = await userSchema.create({
+      username: username,
+      email: email,
+      password: hashedPassword
+    })
+
+    res.status(201).json({
+      message: "User created successfully",
+      user_id: newUser.id
+    })
+
+
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const connectDB = require('./db');
+
+const userRoutes = require('./routes/user');
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/user', userRoutes);
+
+const PORT = process.env.port || 8081;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
