e7e137f3aada1c77c4e392dea7955cdf19eb2e70
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-10-12 18:37:59 -0400

    user login implemented

--- controllers/user.js
+++ controllers/user.js
@@ -25,3 +25,35 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: err.message })
   }
 }
+
+
+exports.login = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+
+    const user = await userSchema.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const passwordCorrect = await bcrypt.compare(password, user.password)
+    if (passwordCorrect) {
+      return res.status(200).json({
+        message: "Login successful."
+      })
+    }
+
+    return res.status(401).json({
+      message: "Invalid credentials"
+    })
+
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
